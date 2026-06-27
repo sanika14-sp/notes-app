6f0d1d2ff8eabf6250841958573f5d821dd5e946
sanika14-sp <gsanikapm14@gmail.com>
2026-06-28 00:53:42 +0530

    add popup created

---
+++ public/notes.js
@@ -0,0 +1,7 @@
+const popup =()=>{
+    let text= document.getElementById("noteBlock");
+    text.style.display = "block"
+}
+ let addBtn = document.getElementById("addBtn")
+ addBtn.addEventListener("click",popup);
+
