f9adc0a5dcdc1e14f6d3e603312e713954a01342
sanika14-sp <gsanikapm14@gmail.com>
2026-07-02 20:37:19 +0530

    initialize Express server and update notes app

--- public/notes.js
+++ public/notes.js
@@ -1,7 +1,19 @@
-const popup =()=>{
-    let text= document.getElementById("noteBlock");
+let notes = [];
+const popup = () => {
+    let text = document.getElementById("noteBlock");
     text.style.display = "block"
 }
- let addBtn = document.getElementById("addBtn")
- addBtn.addEventListener("click",popup);
+const savenote = () => {
+    let title = document.getElementById("noteTitle").value;
+    let context = document.getElementById("noteContent").value;
+    let note = {
+        title,context
+    }
+    notes.push(note);
+    console.log(notes);
+}
+let addBtn = document.getElementById("addBtn")
+addBtn.addEventListener("click", popup);
+let savebtn = document.getElementById("savebtn");
+savebtn.addEventListener("click", savenote);
 
