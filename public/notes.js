67b83f81dca8b9bc55d8cc689c51465b0a59ce49
sanika14-sp <gsanikapm14@gmail.com>
2026-07-14 23:58:05 +0530

    display the task feature added

--- public/notes.js
+++ public/notes.js
@@ -7,10 +7,23 @@ const savenote = () => {
     let title = document.getElementById("noteTitle").value;
     let context = document.getElementById("noteContent").value;
     let note = {
-        title,context
+        title, context
     }
     notes.push(note);
     console.log(notes);
+    displayNotes();
+}
+const displayNotes = () => {
+    let container = document.getElementById("displayNote");
+    container.innerHTML = "";
+   Notes.forEach((note) => {
+        let displayCard = document.createElement("div");
+        displayCard.className = "noteCard";
+        displayCard.innerHTML = `<h3>${note.title}</h3>
+    <p>${note.context}</p>`;
+        container.appendChild(displayCard);
+    });
+
 }
 let addBtn = document.getElementById("addBtn")
 addBtn.addEventListener("click", popup);
