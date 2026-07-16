24c8960cba569664fce306765d5d9ccc6d901298
sanika14-sp <gsanikapm14@gmail.com>
2026-07-16 06:44:04 +0530

    more feature added to the existing buttons

--- public/notes.js
+++ public/notes.js
@@ -12,16 +12,38 @@ const savenote = () => {
     notes.push(note);
     console.log(notes);
     displayNotes();
+    document.getElementById("noteTitle").value = "";
+    document.getElementById("noteContent").value = "";
+    document.getElementById("noteBlock").style.display = "none";
+
 }
 const displayNotes = () => {
     let container = document.getElementById("displayNote");
     container.innerHTML = "";
-   Notes.forEach((note) => {
+    notes.forEach((note, index) => {
         let displayCard = document.createElement("div");
         displayCard.className = "noteCard";
         displayCard.innerHTML = `<h3>${note.title}</h3>
-    <p>${note.context}</p>`;
+        <p>${note.context}</p>`
+        ;
+        displayCard.addEventListener("click", () => {
+            displayCard.classList.toggle("expanded");
+        });
+
+        const deletebtn = () => {
+           const deletebutton = document.createElement("button");
+            deletebutton.className = " deletebtn ";
+            deletebutton.textContent = "🗑";
+            deletebutton.addEventListener("click", () => {
+                event.stopPropagation();
+                notes.splice(index, 1);
+                displayNotes();
+            })
+            displayCard.appendChild(deletebutton);
+        }
+        deletebtn();
         container.appendChild(displayCard);
+
     });
 
 }
@@ -29,4 +51,3 @@ let addBtn = document.getElementById("addBtn")
 addBtn.addEventListener("click", popup);
 let savebtn = document.getElementById("savebtn");
 savebtn.addEventListener("click", savenote);
-
